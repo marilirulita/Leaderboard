0b57dd7d375f7c804ffca4a2f873d59475774755
marysol <marilirulita@gmail.com>
2022-01-25 21:16:24 -0800

    Add html and css basic style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello Developer :D';
-  element.classList.add('hello');
-  
-  return element;
-}
-
-document.body.appendChild(component());
