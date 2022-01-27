3032afc2e98fdda4d045c50287b01153e2f8ef03
marysol <marilirulita@gmail.com>
2022-01-27 14:09:49 -0800

    Add design to web page

--- src/score-display.js
+++ src/score-display.js
@@ -1,9 +1,9 @@
-const displayScores = (obj) => {
+const displayScores = (arr) => {
+  const players = arr.sort((a, b) => b.score - a.score);
   const board = document.getElementById('scores');
   board.innerHTML = '';
-  const playersInfo = obj.result;
 
-  playersInfo.forEach((player) => {
+  players.forEach((player) => {
     const item = document.createElement('li');
     item.innerHTML = `<p>${player.user}: ${player.score}</p>`;
     board.appendChild(item);
@@ -15,8 +15,9 @@ const getAllScores = async () => {
   const request = new Request(requestURL);
   const response = await fetch(request);
   const players = await response.json();
+  const playersInfo = players.result;
 
-  displayScores(players);
+  displayScores(playersInfo);
 };
 
 export { getAllScores as default };
