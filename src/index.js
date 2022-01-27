a36c8eaeb962f5f4dbaacf2346241a89e0141e7a
marysol <marilirulita@gmail.com>
2022-01-27 15:10:55 -0800

    Add newScore function to new score file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import './style.css';
 import getAllScores from './score-display.js';
-import newScore from './new-score.js';
+import addNewScore from './new-score.js';
 
 const refresh = document.getElementById('refresh');
 refresh.addEventListener('click', () => getAllScores());
 
-const inputName = document.getElementById('input-name');
-const inputScore = document.getElementById('input-score');
 const submit = document.getElementById('submit');
-submit.addEventListener('click', () => {
-  if (inputName.value !== '' && inputScore.value !== '') {
-    newScore(inputName.value, inputScore.value);
-    inputName.value = '';
-    inputScore.value = '';
-  }
-});
+submit.addEventListener('click', () => addNewScore());
 
 window.onload = () => getAllScores();
